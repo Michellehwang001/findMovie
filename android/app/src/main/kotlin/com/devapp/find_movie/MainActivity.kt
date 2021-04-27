da936ade9eac8425fb3fcc283f967183ceedb6f5
Hwang Hyejung <jimin_ma@naver.com>
2021-04-27 10:11:38 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/devapp/find_movie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.devapp.find_movie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FindMovie());
+}
+
+class FindMovie extends StatefulWidget {
+  @override
+  _FindMovieState createState() => _FindMovieState();
+}
+
+class _FindMovieState extends State<FindMovie> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
